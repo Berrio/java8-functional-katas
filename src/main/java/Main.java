4904a264fc8ca02b3b7fe9eb941346ff2bbed201
JonatanBerrioRoman <berrioromanjonatan@gmail.com>
2022-05-25 14:07:41 -0500

    kata 3 listo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
 import katas.*;
+import model.MovieList;
+import util.DataUtil;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class Main {
@@ -8,9 +13,12 @@ public class Main {
 
 //        assertEquals(Kata2.execute().size(), 2);
 //        System.out.println("Kata 2 solved!");
-//
-        assertEquals(Kata3.execute().size(), 4);
-        System.out.println("Kata 3 solved!");
+
+//        List<MovieList> movieLists = DataUtil.getMovieLists();
+//        movieLists.get(0).
+
+//        assertEquals(Kata3.execute().size(), 4);
+//        System.out.println("Kata 3 solved!");
 //
 //        assertEquals(Kata4.execute().size(), 4);
 //        System.out.println("Kata 4 solved!");
